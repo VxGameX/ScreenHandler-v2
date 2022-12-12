a24b012661051a506ae38c63a08d8781c7bcf230
VxGameX <69877860+VxGameX@users.noreply.github.com>
2022-12-12 15:49:01 -0400

    Added 'Description' to 'ConfigFile.cs'

--- src/ScreenHandler/Settings/ConfigFile.cs
+++ src/ScreenHandler/Settings/ConfigFile.cs
@@ -6,6 +6,7 @@ public class ConfigFile
     public string Id { get; set; } = null!;
     public Title Title { get; set; } = null!;
     public Body Body { get; set; } = null!;
+    public string? Description { get; set; }
     public IEnumerable<Section>? Sections { get; set; }
     public IEnumerable<Action> Actions { get; set; } = null!;
 }
