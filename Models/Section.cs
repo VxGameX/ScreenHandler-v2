5891b775b0c68706ac1a0cb29121dae33f065cc6
VxGameX <69877860+VxGameX@users.noreply.github.com>
2022-12-19 16:07:52 -0400

    Readded ID to Section

--- Models/Section.cs
+++ Models/Section.cs
@@ -2,6 +2,7 @@ namespace ScreenHandler.Models;
 
 public class Section
 {
+    public string Id { get; set; } = null!;
     public string Label { get; set; } = null!;
     public Input Input { get; set; } = null!;
     public bool Required { get; set; } = default;
