f3afa78e191a3a5f454f90072c803f284a2f8c10
VxGameX <69877860+VxGameX@users.noreply.github.com>
2022-12-20 11:22:59 -0400

    Added actions to ScreenDefinition

--- Models/ScreenDefinition.cs
+++ Models/ScreenDefinition.cs
@@ -5,5 +5,6 @@ public class ScreenDefinition
     public string Id { get; set; } = null!;
     public string Type { get; set; } = null!;
     public string Title { get; set; } = null!;
-    public ICollection<Section> Sections { get; set; } = null!;
+    public IEnumerable<Section> Sections { get; set; } = null!;
+    public IEnumerable<Action> Actions { get; set; } = null!;
 }
